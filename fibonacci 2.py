db7929fca889cadca5930d5500428e2a6be1be49
vandana-eva <92171611+vandana-eva@users.noreply.github.com>
2022-08-18 19:04:37 +0530

    Add files via upload

---
+++ fibonacci 2.py
@@ -0,0 +1,16 @@
+from tkinter import*
+
+root=Tk()
+
+root.title("Fibonacci")
+
+root.geometry("1000x1000")
+
+root.mainloop()
+
+label_series=Label(root,text="fibonacci series: ")
+
+label_flower=Label(root)
+
+label_spiral=Label(root)
+
